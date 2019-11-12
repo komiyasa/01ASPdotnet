baf6677b1345d0c8f0ba738fb9266f1118fe320d
komiyasa <komiyasa@microsoft.com>
2019-11-12 18:25:44 +0900

    Forth Commot

---
+++ 02CustomVisionWebApp/v1/CVWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CVWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
